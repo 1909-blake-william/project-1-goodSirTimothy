cc3d36855b1c21fdff956d034d8e3cea0a9fdfa0
Tim C <goodSirTimothy@users.noreply.github.com>
2019-11-20 14:45:34 -0500

    Let's see if this work this time
    Added Angular and fixed where the git repo file was located.

---
+++ src/main/java/revature/dao/UserDao.java
@@ -0,0 +1,38 @@
+package revature.dao;
+
+import revature.model.User;
+
+/**
+ * An interface for all User methods that handle SQL queries
+ * @author Tim Clifton
+ *
+ */
+public interface UserDao {
+	
+	/**
+	 * The current user that has logged into the Database
+	 */
+	UserDao currentUser = new UserDaoImpl();
+	
+	/**
+	 * check if the user exists.
+	 * @param username = the name entered
+	 * @return return true or false if the information is correct or not
+	 */
+	boolean findUsername(String username);
+	
+	/**
+	 * find the username by username and password
+	 * @param username = the username inputed
+	 * @param password = the password inputed
+	 * @return return a User object if the user is found. Else, return null.
+	 */
+	User findByUsernameAndPassword(String username, String password);
+	
+	/**
+	 * return the current user
+	 * @return User
+	 */
+	User currentUser();
+	
+}
